c5893f808112d13ef00df5a2a6efb254a35383e4
Ákos Koszták <kokoszta77@gmail.com>
2023-03-25 22:17:23 +0100

    GUI: A kezdetek

---
+++ ClientApp/src/components/Login.js
@@ -0,0 +1,24 @@
+﻿import React, { Component } from 'react';
+import './Login.css'
+
+export class Login extends Component {
+    static displayName = Login.name;
+
+    render() {
+        return (
+            <div>
+                <div className="bg">
+                    <h1>Bejelentkezés</h1>
+                    <fieldset className="input-field">
+                        <legend>Felhasználónév:</legend>
+                        <input type="text"></input>
+                    </fieldset>
+                    <fieldset className="input-field">
+                        <legend>Jelszó:</legend>
+                        <input type="text"></input>
+                    </fieldset>
+                </div>
+            </div>
+        );
+    }
+}
--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -21,27 +21,30 @@ export class NavMenu extends Component {
     });
   }
 
-  render() {
-    return (
-      <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-          <NavbarBrand tag={Link} to="/">rendszerfejlesztes_beadando</NavbarBrand>
-          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-          <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-            <ul className="navbar-nav flex-grow">
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
-              </NavItem>
-            </ul>
-          </Collapse>
-        </Navbar>
-      </header>
-    );
-  }
+    render() {
+        return (
+            <header>
+                <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
+                    <NavbarBrand className="menu-title" tag={Link} to="/">Napelem Rendszer</NavbarBrand>
+                    <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+                    <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
+                        <ul className="navbar-nav flex-grow">
+                        <NavItem>
+                            <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} className="text-dark" to="/login">Login</NavLink>
+                        </NavItem>
+                        </ul>
+                    </Collapse>
+                </Navbar>
+            </header>
+        );
+    }
 }
