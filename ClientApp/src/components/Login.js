1025611acd662a834e5ce3a48b1ce4606f481e91
Ákos Koszták <kokoszta77@gmail.com>
2023-03-27 23:32:30 +0200

    Login gomb behelyezése (még nem működik)

--- ClientApp/src/components/Login.js
+++ ClientApp/src/components/Login.js
@@ -1,22 +1,28 @@
 ﻿import React, { Component } from 'react';
-import './Login.css'
+import { useNavigate } from "react-router-dom";
+import './Login.css';
 
 export class Login extends Component {
     static displayName = Login.name;
+
+    login = () => {
+        useNavigate("/home");
+    }
 
     render() {
         return (
             <div>
                 <div className="bg">
                     <h1>Bejelentkezés</h1>
-                    <fieldset className="input-field">
+                    <fieldset className="input-field" id="name">
                         <legend>Felhasználónév:</legend>
                         <input type="text"></input>
                     </fieldset>
-                    <fieldset className="input-field">
+                    <fieldset className="input-field" id="pass">
                         <legend>Jelszó:</legend>
                         <input type="text"></input>
                     </fieldset>
+                    <button onClick={this.login}>Bejelentkezés</button>
                 </div>
             </div>
         );
