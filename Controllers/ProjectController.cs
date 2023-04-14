b196db809f1a1266aa099a843a5596cb68adb518
Adrián <adreszka2001@gmail.com>
2023-04-14 17:14:43 +0200

    Project funkcióknak előkészítése

---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using rendszerfejlesztes_beadando.Repositories;
+
+namespace rendszerfejlesztes_beadando.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProjectController : Controller
+    {
+        private readonly ProjectRepository repo;
+
+        public ProjectController(ProjectRepository repo)
+        {
+            this.repo = repo;
+        }
+    }
+}
