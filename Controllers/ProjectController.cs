0ae1f6e7696212c201b9fc81872afe4fed41a679
Andris-tech <huszar.andris2002@gmail.com>
2023-04-22 19:01:04 +0200

    A4 és A5 funkciók megvalósítása

--- Controllers/ProjectController.cs
+++ Controllers/ProjectController.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using rendszerfejlesztes_beadando.Models;
+using rendszerfejlesztes_beadando.Models.Entities;
 using rendszerfejlesztes_beadando.Repositories;
 
 namespace rendszerfejlesztes_beadando.Controllers
 {
-    [Route("[controller]")]
+    [Route("[controller]/[action]")]
     [ApiController]
     public class ProjectController : Controller
     {
@@ -19,6 +20,33 @@ namespace rendszerfejlesztes_beadando.Controllers
         public async Task<ActionResult<bool>> addNewProject([FromBody] NewProject project) 
         {
             var result = await repo.addNewProject(project);
+            return Ok(result);
+        }
+        [HttpPost]
+        public async Task<ActionResult<bool>> AddWorkTimeAndFee([FromBody] WorkTimeAndFee timeandfee)
+        {
+            var results = await repo.AddWorkTimeAndFee(timeandfee);
+            return Ok(results);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<bool>> AddComponentToProject([FromBody] AddComponentToProject component)
+        {
+            var result = await repo.AddComponentToProject(component);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Project>>> GetProjects()
+        {
+            var result = await repo.GetProjects();
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProjectComponent>>> GetProjectComponents()
+        {
+            var result = await repo.GetProjectComponents();
             return Ok(result);
         }
     }
