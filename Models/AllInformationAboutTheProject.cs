5eae9002af099e68a3c7a05937c0300d0acd0aa2
Bence <bencus0110@gmail.com>
2023-05-17 12:35:38 +0200

    Projekt listázás raktárosnál módosítva

--- Models/AllInformationAboutTheProject.cs
+++ Models/AllInformationAboutTheProject.cs
@@ -5,10 +5,10 @@ namespace rendszerfejlesztes_beadando.Models
     public class AllInformationAboutTheProject
     {
         public string StatusName { get; set; }
-        public ProjectModel Project { get; set; }
-        public List<StoreComponent> MissingCompsFromProjects { get; set; }
-        public List<StoreComponent> ReservedComponents { get; set; }
-        public CustomerModel Customer { get; set; }
+        //public ProjectModel Project { get; set; }
+        //public List<StoreComponent> MissingCompsFromProjects { get; set; }
+        //public List<StoreComponent> ReservedComponents { get; set; }
+        //public CustomerModel Customer { get; set; }
         public List<PathData> ProjectComponents { get; set; }
     }
 }
